ee3305078fd102f265987287364afd38dabf01f3
Afnan Alhabi <afnan.m.alhabi@gmail.com>
2022-10-27 12:04:31 +0300

    Initial Commit

---
+++ testGithub/testGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGithubApp.swift
+//  testGithub
+//
+//  Created by Afnan on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct testGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
